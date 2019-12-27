3987ce1931169958953188033b71c365685b1dec
yaok <yaok@pingansec.com>
2019-12-27 23:32:35 +0800

    模拟ssh远程执行命令

---
+++ 计算机网络编程/05模拟ssh远程执行命令/client.py
@@ -0,0 +1,24 @@
+# coding:utf8
+import socket
+
+
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+phone.connect(('127.0.0.1', 8080))
+
+
+while True:
+
+    # 1、发命令
+    cmd = raw_input('>>:')
+
+    if not cmd:
+        continue
+    phone.send(cmd.encode('utf-8'))
+
+    # 拿到命令结果并打印
+    data = phone.recv(1024)    # 1024是一个坑
+    print data
+
+# 4、关机
+phone.close()
