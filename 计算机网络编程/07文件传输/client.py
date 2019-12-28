f626820a252901968be5b0d4ea541ad2d1a3bb67
yaok <yaok@pingansec.com>
2019-12-28 22:10:56 +0800

    文件传输

---
+++ 计算机网络编程/07文件传输/client.py
@@ -0,0 +1,50 @@
+# coding:utf8
+import socket
+import struct
+import json
+import os
+
+
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+phone.connect(('127.0.0.1', 8080))
+
+download_dir = os.path.join(os.getcwd(), 'download')
+
+while True:
+
+    # 1、发命令
+    cmd = raw_input('>>:')
+
+    if not cmd:
+        continue
+    phone.send(cmd.encode('utf-8'))
+
+    # 2、以写的方式打开一个新文件，接收服务端发来的文件内容写入客户端的新文件
+
+    # 第一步：先收报头的长度
+    obj = phone.recv(4)
+    header_size = struct.unpack('i', obj)[0]
+
+    # 第二步：再收报头
+    header_byte = phone.recv(header_size)
+
+    # 第三步：从报头中解析出对真实数据的描述信息
+    header_json = header_byte.decode('utf-8')
+    header_dic = json.loads(header_json)
+    print header_dic
+    total_size = header_dic['total_size']
+    file_name = header_dic['filename']
+
+    # 第三步：接受真实数据
+    with open(os.path.join(download_dir, file_name.encode('utf-8')), 'wb') as f:
+        recv_size = 0
+        recv_data = ''
+        while recv_size < total_size:
+            line = phone.recv(1024)
+            f.write(line)
+            recv_size += len(line)
+            print '总大小%d，已下载%d' % (total_size,recv_size)
+
+# 4、关机
+phone.close()
---
+++ 计算机网络编程/07文件传输/server.py
@@ -0,0 +1,61 @@
+# coding:utf8
+import socket
+import os
+import struct
+import json
+
+
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+phone.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+phone.bind(('127.0.0.1', 8080))
+
+phone.listen(5)
+
+share_dir = os.path.join(os.getcwd(), 'share')
+
+while True:     # 链接循环
+    conn, client_addr = phone.accept()
+
+    while True:
+
+        # 1、接受命令
+        res = conn.recv(1024)    # get a.txt
+        if not res:
+            break
+
+        # 2、解析命令，提取相应命令参数
+        cmds = res.decode('utf-8').split()     # ['get','a.txt']
+        filename = cmds[1]
+
+        # 3、以读的方式打开a.txt，读取文件内容发送给客户端
+
+        # 第一步：制作固定长度的报头
+        header_dic = {
+            'filename': filename,
+            'md5': 'xxxxxxxx',
+            'total_size': os.path.getsize('%s/%s' % (share_dir, filename.encode('utf-8')))
+        }
+        header_json = json.dumps(header_dic)
+
+        header_bytes = header_json.encode('utf-8')
+
+        # 第二步：先发送报头的长度
+        conn.send(struct.pack('i', len(header_bytes)))
+
+        # 第三步：再发报头
+        conn.send(header_bytes)
+
+        # 第四步：再发送真实数据
+        with open(os.path.join(share_dir, filename.encode('utf-8')), 'rb') as f:
+            # conn.send(f.read())
+            for line in f:
+                conn.send(line)
+
+    # 6、挂电话
+    conn.close()
+
+
+# 7、关机
+phone.close()
