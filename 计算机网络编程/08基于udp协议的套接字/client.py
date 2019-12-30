ad90ae9f1bbb81a3aa9118671cc8f5b6da51956b
yaokun <yaok@pingansec.com>
2019-12-30 13:38:09 +0800

    修改udp协议

--- 计算机网络编程/08基于udp协议的套接字/client.py
+++ 计算机网络编程/08基于udp协议的套接字/client.py
@@ -5,7 +5,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     msg = raw_input('>>').strip()
 
-    client.sendto(msg.encode('utf-8'), ('127.0.0.1', 8080))
+    client.sendto(msg, ('127.0.0.1', 8080))
 
     data, server_addr = client.recvfrom(1024)
 
