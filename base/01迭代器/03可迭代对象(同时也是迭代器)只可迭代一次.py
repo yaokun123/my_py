ae9cfb33a6884b76fd5c522a5361e09de0204a6a
yaokun <yaok@pingansec.com>
2021-03-23 15:54:47 +0800

    03可迭代对象(同时也是迭代器)只可迭代一次

--- base/01迭代器/03可迭代对象(同时也是迭代器)只可迭代一次.py
+++ base/01迭代器/03可迭代对象(同时也是迭代器)只可迭代一次.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import time
-from collections import Iterable,Iterator
+from collections import Iterable, Iterator
 
 
 class Classmate(object):
@@ -20,7 +20,6 @@ class Classmate(object):
 
     def next(self):
         if self.current_num >= len(self.names):
-            # 抛出异常通知迭代结束，for循环自动就停止了
             raise StopIteration
         res = self.names[self.current_num]
         self.current_num += 1
@@ -32,22 +31,24 @@ classmate.add('test1')
 classmate.add('test2')
 classmate.add('test3')
 
-# for一个可迭代对象，会去调用迭代器的next()方法
-for name in classmate:
-    print(name)
-    time.sleep(1)
 
 # 一个对象是迭代器那么它一定可以迭代
 # 一个对象可以迭代，不一定是迭代器
-print(isinstance(classmate, Iterable))
-print(isinstance(classmate, Iterator))
+print "判断classmate是不是可迭代对象", isinstance(classmate, Iterable)
+print "判断classmate是不是迭代器", isinstance(classmate, Iterator)
+print "============================================================"
 
-
-# 迭代器呢，一次for循环就没有了，而可迭代对象呢，任你for一万次都会输出
-print('-------------')
 for name in classmate:
     print(name)
     time.sleep(1)
+
+print "============================================================"
+# 迭代器呢，一次for循环就没有了，而可迭代对象呢，任你for一万次都会输出
+# classmate是可迭代对象（但同时也是迭代器），所以不能多次迭代
+for name in classmate:
+    print(name)
+    time.sleep(1)
+
 
 # range(10)返回的是一个列表，占用空间大，存储的是数据
 # xrange(10)返回的是一个迭代器，占用空间较小，存储的是生成数据的方式，而不是数据
