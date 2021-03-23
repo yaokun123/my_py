92f97d22f5e42ecaa6d25c7ecd10008ad373540e
yaokun <yaok@pingansec.com>
2021-03-23 15:20:27 +0800

    print去掉括号打印

--- base/01迭代器/01迭代器.py
+++ base/01迭代器/01迭代器.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import time
-from collections import Iterable,Iterator
+from collections import Iterable, Iterator
 
 
 class Classmate(object):
@@ -34,17 +34,17 @@ classmate.add('test1')
 classmate.add('test2')
 classmate.add('test3')
 
-print("判断classmate是否是可迭代对象：", isinstance(classmate, Iterable))
+print "判断classmate是否是可迭代对象：", isinstance(classmate, Iterable)
 
 # iter()方法会获取到迭代器
 classmate_iterator = iter(classmate)
 
-print("判断classmate_iterator是否是迭代器：", isinstance(classmate_iterator, Iterator))
+print "判断classmate_iterator是否是迭代器：", isinstance(classmate_iterator, Iterator)
 
 # next()方法会调用迭代器的next()方法
-print(next(classmate_iterator))
+print next(classmate_iterator)
 
 # for一个可迭代对象，会去调用迭代器的next()方法
 for name in classmate:
-    print(name)
+    print name
     time.sleep(1)
