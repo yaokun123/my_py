668da1166ae509ae5860f390e3e42501bd686a5a
yaok <yaok@pingansec.com>
2021-11-28 14:21:54 +0800

    Iterable和Iterator

--- base/01迭代器/04迭代器的应用.py
+++ base/01迭代器/04迭代器的应用.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-
+from collections import Iterable,Iterator
 nums = list()
 
 a = 0
@@ -10,9 +10,19 @@ while i < 10:
     nums.append(a)
     a, b = b, a+b
     i += 1
-
+print "nums is a list check is Iterable:", isinstance(nums, Iterable)
+print "nums is a list check is Iterator:", isinstance(nums, Iterator)
+print "=============================================================="
+print "可迭代对象（非迭代器对象）可以任意迭代"
+str1 = ""
 for num in nums:
-    print num
+    str1 += str(num) + ','
+print(str1)
+str1 = ""
+for num in nums:
+    str1 += str(num) + ','
+print str1
+print "=============================================================="
 
 
 print("使用迭代器")
@@ -39,8 +49,22 @@ class Fib(object):
 
 
 fibo = Fib(10)
+print "check fibo is Iterable:", isinstance(fibo, Iterable)
+print "check fibo is Iterator:", isinstance(fibo, Iterator)
+str1 = ""
 for num in fibo:
-    print num
+    str1 += str(num) + ','
+print str1
+
+# print("第二次迭代：")
+# str1 = ""
+# fibo.current_num = 0
+# fibo.a = 0
+# fibo.b = 1
+# for num in fibo:
+#     str1 += str(num) + ','
+# print str1
+
 
 # list()和tuple()底层就是使用迭代器
 
