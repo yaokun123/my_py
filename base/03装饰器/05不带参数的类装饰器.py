22678c6db22d933bdce4e101f8424bf7a5859a7d
姚坤 <yaok@pingansec.com>
2022-07-13 17:07:00 +0800

    装饰器

--- base/03装饰器/05不带参数的类装饰器.py
+++ base/03装饰器/05不带参数的类装饰器.py
@@ -1,10 +1,13 @@
 # coding:utf-8
 
+# 类中的__call__函数，可以让类的实例像函数调用样被调用
+
 """
 基于类装饰器的实现，必须实现 __call__ 和 __init__两个内置函数。
 __init__：接收被装饰函数
 __call__：实现装饰逻辑
 """
+from collections import Callable
 
 
 class Logger(object):
@@ -17,7 +20,17 @@ class Logger(object):
 
 
 @Logger
-def say():
+def say():      # 等见于say = Logger(say)
     print "hello world"
 
+
+print "===================================="
+# 此时的say 其实是一个Logger对象，而不是函数
+print say
+
+# 实现__call__函数的类的对象是可调用对象，没有实现就是不可调用对象
+print isinstance(say, Callable)
+print "===================================="
+
+# 之所以可以直接调用是因为实现了__call__函数。调用对象的时候会去找__call__函数
 say()
--- base/03装饰器/06带参数的类装饰器.py
+++ base/03装饰器/06带参数的类装饰器.py
@@ -9,17 +9,23 @@ __call__：接受被装饰函数，实现装饰逻辑
 
 class Logger(object):
     def __init__(self, level='INFO'):
+        print "__init__"
         self.level = level
 
     def __call__(self, func):   # 接受函数
+        print "__call__"
+
         def wrapper(*args, **kwargs):
             print self.level + ":" + "before:"
             func(*args, **kwargs)
         return wrapper          # 返回函数
 
 
-@Logger("notice")
+@Logger("notice")       # 等见于say = Logger("notice")()
 def say():
     print "hello world"
+
+# 此时的say是一个函数<function wrapper at 0x10aff5320>
+print say
 
 say()
