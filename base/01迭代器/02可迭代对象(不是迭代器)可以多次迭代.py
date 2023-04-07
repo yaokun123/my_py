29bf027fa3694bf47a2c5efeda01c1de4abc4ca9
姚坤 <yaok@pingansec.com>
2023-04-07 18:34:22 +0800

    迭代器与可迭代对象

--- base/01迭代器/02可迭代对象(不是迭代器)可以多次迭代.py
+++ base/01迭代器/02可迭代对象(不是迭代器)可以多次迭代.py
@@ -1,39 +1,38 @@
 # coding:utf-8
-import time
 
 
 class Classmate(object):
+    """
+    可迭代对象的类
+    """
     def __init__(self):
         self.names = list()
 
-    def add(self, name):
-        self.names.append(name)
+    def add(self, name_):
+        self.names.append(name_)
 
     def __iter__(self):
-        """
-        如果想要一个对象称为一个 可以迭代的对象，即可以使用for，那么必须实现__iter__方法
-        :return:(需要是一个对迭代器的引用)
-        """
         return ClassIterator(self)
 
 
 class ClassIterator(object):
     """
-    迭代器必须实现__iter__和next方法
+    迭代器的类
     """
+    def __init__(self, obj):
+        self.obj = obj
+        self.current_num = 0
+
     def __iter__(self):
         pass
 
     def next(self):
         if self.current_num >= len(self.obj.names):
-            raise StopIteration  # 抛出异常通知迭代结束，for循环自动就停止了
+            # for只判断了StopIteration的异常，没有判断其他异常，所以这里只能抛出StopIteration的异常
+            raise StopIteration
         res = self.obj.names[self.current_num]
         self.current_num += 1
         return res
-
-    def __init__(self, obj):
-        self.obj = obj
-        self.current_num = 0
 
 
 classmate = Classmate()
@@ -41,13 +40,10 @@ classmate.add('test1')
 classmate.add('test2')
 classmate.add('test3')
 
-# for一个可迭代对象，会去调用迭代器的next()方法
 for name in classmate:
     print(name)
-    time.sleep(1)
 
 print "================================================"
-# classmate是可迭代对象（但是不是迭代器），所以可以多次迭代
+# 每次获取迭代器的时候都获取的是新的迭代器对象（迭代器的计数器都是新的）所以可以多次迭代
 for name in classmate:
     print(name)
-    time.sleep(1)
