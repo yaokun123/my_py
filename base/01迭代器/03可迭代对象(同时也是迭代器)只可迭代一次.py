b10ba322cd0db31f74c20239d23b0427202196ea
姚坤 <yaok@pingansec.com>
2023-04-07 18:57:22 +0800

    迭代器与可迭代对象

--- base/01迭代器/03可迭代对象(同时也是迭代器)只可迭代一次.py
+++ base/01迭代器/03可迭代对象(同时也是迭代器)只可迭代一次.py
@@ -1,21 +1,14 @@
 # coding:utf-8
-import time
-from collections import Iterable, Iterator
-
 
 class Classmate(object):
     def __init__(self):
         self.names = list()
         self.current_num = 0
 
-    def add(self, name):
-        self.names.append(name)
+    def add(self, name_):
+        self.names.append(name_)
 
     def __iter__(self):
-        """
-        如果想要一个对象称为一个 可以迭代的对象，即可以使用for，那么必须实现__iter__方法
-        :return:(需要是一个对迭代器的引用)
-        """
         return self
 
     def next(self):
@@ -25,6 +18,9 @@ class Classmate(object):
         self.current_num += 1
         return res
 
+    def reset(self):
+        self.current_num = 0
+
 
 classmate = Classmate()
 classmate.add('test1')
@@ -32,23 +28,64 @@ classmate.add('test2')
 classmate.add('test3')
 
 
-# 一个对象是迭代器那么它一定可以迭代
-# 一个对象可以迭代，不一定是迭代器
-print "判断classmate是不是可迭代对象", isinstance(classmate, Iterable)
-print "判断classmate是不是迭代器", isinstance(classmate, Iterator)
-print "============================================================"
-
 for name in classmate:
     print(name)
-    time.sleep(1)
 
 print "============================================================"
-# 迭代器呢，一次for循环就没有了，而可迭代对象呢，任你for一万次都会输出
-# classmate是可迭代对象（但同时也是迭代器），所以不能多次迭代
+# 每次获取迭代器的时候都获取的是自己（迭代器的计数器复用）所以不能多次迭代
+class_iterator_1 = iter(classmate)
+class_iterator_2 = iter(classmate)
+print class_iterator_1
+print class_iterator_2
+print "不能多次迭代。。。"
 for name in classmate:
     print(name)
-    time.sleep(1)
+
+print "============================================================"
+print "重置计数器，可以再次迭代"
+classmate.reset()
+for name in classmate:
+    print(name)
 
 
+print "============================================================"
 # range(10)返回的是一个列表，占用空间大，存储的是数据
+range_list = range(10)
+print range_list, type(range_list)
+
 # xrange(10)返回的是一个可迭代对象，占用空间较小，存储的是生成数据的方式，而不是具体的数据
+xrange_iterable = xrange(10)
+print xrange_iterable, type(xrange_iterable)
+
+
+class XrangeIterable(object):
+    def __init__(self, num):
+        self.num = num
+
+    def __iter__(self):
+        return XrangeIterator(self.num)
+
+
+class XrangeIterator(object):
+    def __iter__(self):
+        pass
+
+    def __init__(self, num):
+        self.pos = 0
+        self.num = num
+
+    def next(self):
+        if self.pos < self.num:
+            tmp = self.pos
+            self.pos += 1
+            return tmp
+        else:
+            raise StopIteration
+
+
+def my_xrange(num):
+    return XrangeIterable(num)
+
+
+my_xrange_iterable = my_xrange(10)
+print my_xrange_iterable, type(my_xrange_iterable)
